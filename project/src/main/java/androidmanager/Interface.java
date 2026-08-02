09c1471e123c4af02c021bb59843b85252b9bd2f
Gaboxey ★ <96131201+GaboWuz@users.noreply.github.com>
2026-08-02 20:34:08 -0300

    yuh, eu não sou nerd pra fazer esse tipo de coisa.

--- project/src/main/java/androidmanager/Interface.java
+++ project/src/main/java/androidmanager/Interface.java
@@ -4,60 +4,182 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.Settings;
 import org.haxe.extension.Extension;
 import org.haxe.lime.HaxeObject;
 
-public class Interface extends Extension {
-    public static void alert(final String title, final String msg, final String btn, final HaxeObject callback) {
-    mainActivity.runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-            AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity, android.R.style.Theme_DeviceDefault_Dialog_Alert);
-            
-            builder.setTitle(title);
-            builder.setMessage(msg);
-            builder.setPositiveButton(btn, (dialog, which) -> {
-                if (callback != null) callback.call("onTrigger", new Object[]{});
-            });
-            builder.setCancelable(false);
-            builder.show();
-        }
-    });
-}
+public class Interface extends Extension
+{
+    private static final String MANAGE_ALL_FILES_ACTION =
+        "android.settings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION";
 
-public static void confirm(final String title, final String msg, final String yes, final String no, final HaxeObject onYes, final HaxeObject onNo) {
-    mainActivity.runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-            AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity, android.R.style.Theme_DeviceDefault_Dialog_Alert);
-            builder.setTitle(title);
-            builder.setMessage(msg);
-            
-            builder.setPositiveButton(yes, (dialog, which) -> {
-                if (onYes != null) onYes.call("onTrigger", new Object[]{});
-            });
-            
-            builder.setNegativeButton(no, (dialog, which) -> {
-                if (onNo != null) onNo.call("onTrigger", new Object[]{});
-            });
-            
-            builder.setCancelable(false);
-            builder.show();
-        }
-    });
-}
+    private static final String SETTINGS_ACTION =
+        "android.settings.SETTINGS";
 
-public static void navigate(final String action, final int code) {
-      mainActivity.runOnUiThread(() -> {
-            try {
-                Intent intent = new Intent(action);
-                if (action.equals(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)) {
-                    intent.setData(Uri.parse("package:" + mainContext.getPackageName()));
+    public static void alert(
+        final String title,
+        final String msg,
+        final String btn,
+        final HaxeObject callback
+    )
+    {
+        mainActivity.runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                AlertDialog.Builder builder =
+                    new AlertDialog.Builder(
+                        mainActivity,
+                        android.R.style.Theme_DeviceDefault_Dialog_Alert
+                    );
+
+                builder.setTitle(title);
+                builder.setMessage(msg);
+
+                builder.setPositiveButton(
+                    btn,
+                    new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(
+                            DialogInterface dialog,
+                            int which
+                        )
+                        {
+                            if (callback != null)
+                            {
+                                callback.call(
+                                    "onTrigger",
+                                    new Object[] {}
+                                );
+                            }
+                        }
+                    }
+                );
+
+                builder.setCancelable(false);
+                builder.show();
+            }
+        });
+    }
+
+    public static void confirm(
+        final String title,
+        final String msg,
+        final String yes,
+        final String no,
+        final HaxeObject onYes,
+        final HaxeObject onNo
+    )
+    {
+        mainActivity.runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                AlertDialog.Builder builder =
+                    new AlertDialog.Builder(
+                        mainActivity,
+                        android.R.style.Theme_DeviceDefault_Dialog_Alert
+                    );
+
+                builder.setTitle(title);
+                builder.setMessage(msg);
+
+                builder.setPositiveButton(
+                    yes,
+                    new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(
+                            DialogInterface dialog,
+                            int which
+                        )
+                        {
+                            if (onYes != null)
+                            {
+                                onYes.call(
+                                    "onTrigger",
+                                    new Object[] {}
+                                );
+                            }
+                        }
+                    }
+                );
+
+                builder.setNegativeButton(
+                    no,
+                    new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(
+                            DialogInterface dialog,
+                            int which
+                        )
+                        {
+                            if (onNo != null)
+                            {
+                                onNo.call(
+                                    "onTrigger",
+                                    new Object[] {}
+                                );
+                            }
+                        }
+                    }
+                );
+
+                builder.setCancelable(false);
+                builder.show();
+            }
+        });
+    }
+
+    public static void navigate(
+        final String action,
+        final int code
+    )
+    {
+        mainActivity.runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Intent intent = new Intent(action);
+
+                    if (MANAGE_ALL_FILES_ACTION.equals(action))
+                    {
+                        intent.setData(
+                            Uri.parse(
+                                "package:"
+                                + mainContext.getPackageName()
+                            )
+                        );
+                    }
+
+                    mainActivity.startActivityForResult(
+                        intent,
+                        code
+                    );
                 }
-                mainActivity.startActivityForResult(intent, code);
-            } catch (Exception e) {
-                mainActivity.startActivityForResult(new Intent(Settings.ACTION_SETTINGS), code);
+                catch (Exception error)
+                {
+                    try
+                    {
+                        Intent fallback =
+                            new Intent(SETTINGS_ACTION);
+
+                        mainActivity.startActivityForResult(
+                            fallback,
+                            code
+                        );
+                    }
+                    catch (Exception fallbackError)
+                    {
+                        fallbackError.printStackTrace();
+                    }
+                }
             }
         });
     }
